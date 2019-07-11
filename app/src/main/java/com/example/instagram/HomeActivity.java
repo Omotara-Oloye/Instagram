6bc17b1e6c56e9d284e4364e71fb4f11995b1a46
Omotara Oloye <ooloye@fb.com>
2019-07-11 13:24:21 -0700

    Can post pictures

--- app/src/main/java/com/example/instagram/HomeActivity.java
+++ app/src/main/java/com/example/instagram/HomeActivity.java
@@ -1,14 +1,22 @@
 package com.example.instagram;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.instagram.models.Post;
 import com.parse.FindCallback;
@@ -24,8 +32,12 @@ public class HomeActivity extends AppCompatActivity {
 
     private static final String imagePath = "";
     private EditText descriptionInput;
-    private Button createButton;
-    private Button refreshButton;
+    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
+    private ImageView ivPhoto;
+    private Button postButton;
+    private Button submitButton;
+    private static final String photoFileName = "photo.jpg";
+    File photoFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,26 +47,37 @@ public class HomeActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         descriptionInput = findViewById(R.id.description_et);
-        createButton = findViewById(R.id.create_btn);
-        refreshButton = findViewById(R.id.refresh_btn);
+        postButton = findViewById(R.id.takePhoto);
+        submitButton = findViewById(R.id.submit);
+        ivPhoto = findViewById(R.id.ivPhoto);
 
-        createButton.setOnClickListener(new View.OnClickListener() {
+        loadTopPosts();
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final String description = descriptionInput.getText().toString();
                 final ParseUser user = ParseUser.getCurrentUser();
+                if(photoFile != null || ivPhoto.getDrawable() != null) {
+                    createPost(description, photoFile, user);
+                }else{
+                    Log.e("HomeActivity", "No photo selected");
+                    Toast.makeText(HomeActivity.this, "Add a photo to make a post!", Toast.LENGTH_LONG).show();
+                    return;
+                }
 
-                final File file = new File(imagePath);
-                final ParseFile parseFile = new ParseFile(file);
 
-                createPost(description, parseFile, user);
+//                final File file = new File(imagePath);
+//                final ParseFile parseFile = new ParseFile(file);
+//
+
             }
         });
 
-        refreshButton.setOnClickListener(new View.OnClickListener() {
+        postButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loadTopPosts();
+                onLaunchCamera(v);
             }
         });
 
@@ -70,40 +93,85 @@ public class HomeActivity extends AppCompatActivity {
                 ParseUser currentUser = ParseUser.getCurrentUser();
             }
         });
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Come Back Soon!", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
-
 
     }
 
-    private void createPost(String description, ParseFile imageFile, ParseUser user){
+
+    private void createPost(String description, File photoFile, ParseUser user){
         final Post newPost = new Post();
         newPost.setDescription(description);
-        newPost.setImage(imageFile);
+        newPost.setImage(new ParseFile(photoFile));
         newPost.setUser(user);
 
         newPost.saveInBackground(new SaveCallback() {
             @Override
             public void done(ParseException e) {
                 if(e == null){
-                    Log.d("HomeActivity", "Create Post Sucess!");
+                    Log.d("HomeActivity", "Post: Success!!");
+                    descriptionInput.setText("");
+                    ivPhoto.setImageResource(0);
+
                 }else{
+                    Log.d("HomeActivity","Error while saving photo");
                     e.printStackTrace();
+                    return;
                 }
             }
         });
+    }
+    public void onLaunchCamera(View view) {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference to access to future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(HomeActivity.this, "com.codepath.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+    public File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), "HomeActivity");
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d("HomeActivity", "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+        return file;
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+                ivPhoto.setImageBitmap(takenImage);
+            } else { // Result was a failure
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     private void loadTopPosts(){
         final Post.Query postsQuery = new Post.Query();
         postsQuery.getTop().withUser();
-
-
         postsQuery.findInBackground(new FindCallback<Post>() {
             @Override
             public void done(List<Post> objects, ParseException e) {
